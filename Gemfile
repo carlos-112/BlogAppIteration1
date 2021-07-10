7cc1d6a4713fcead150d3732d158a005b28b913b
Carlos Franco <me@cfrco.com>
2021-07-09 18:28:47 -0700

    Last Iteration

--- Gemfile
+++ Gemfile
@@ -6,6 +6,9 @@ ruby '3.0.1'
 # Annotate models
 gem 'annotate'
 
+# Fix div_for error
+gem 'record_tag_helper'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.3', '>= 6.1.3.2'
 # Use sqlite3 as the database for Active Record
--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require_relative "boot"
 
 require "rails/all"
 
+require_relative '../lib/timer'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -10,6 +12,8 @@ module Blog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+
+    config.middleware.insert_before(Rack::Sendfile, Timer)
 
     # Configuration for the application, engines, and railties goes here.
     #
